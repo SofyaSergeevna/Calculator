46145965d866e6d784e2a10b4317e5ba0ceb2a93
SofyaSergeevna <sofya.volkova.1999@.mail.ru>
2021-01-13 23:54:50 +0300

    Progect init

---
+++ ArithmeticOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My_personal_calculator
+{
+    public class ArithmeticOperations
+    {
+        public int Sum(int firstValue, int secondValue)
+        {
+            int result = firstValue + secondValue;
+            return result;
+        }
+        
+        public int Minus(int firstValue, int secondValue)
+        {
+            int result = firstValue - secondValue;
+            return result;
+        }
+
+        public int Multiplication(int firstValue, int secondValue)
+        {
+            int result = firstValue * secondValue;
+            return result;
+        }
+
+        public decimal Divide(int firstValue, int secondValue)
+        {
+            decimal result = Convert.ToDecimal(firstValue) / Convert.ToDecimal(secondValue);
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace My_personal_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter first value.");
+            int firstValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please, enter second value.");
+            int secondValue = Convert.ToInt32(Console.ReadLine());
+
+            var operations = new ArithmeticOperations();
+
+            var resultSum = operations.Sum(firstValue, secondValue);
+            Console.WriteLine(resultSum);
+
+            var resultMinus = operations.Minus(firstValue, secondValue);
+            Console.WriteLine(resultMinus);
+
+            var resultMultiplication = operations.Multiplication(firstValue, secondValue);
+            Console.WriteLine(resultMultiplication);
+
+            var resultDivide = operations.Divide(firstValue, secondValue);
+            Console.WriteLine(resultDivide);
+        }
+    }
+}
